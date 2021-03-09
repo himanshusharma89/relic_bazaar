d43b1e9a05869404cc5f54eeb61cf6a258ac4adf
Pawan <pawan.singh.co19@nsut.ac.in>
2021-03-10 03:38:48 +0530

    added product model

---
+++ lib/model/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final String text;
+  final String owner;
+  final String amount;
+  final String image;
+  final String seller;
+  final int height;
+
+  Product({
+    this.text,
+    this.owner,
+    this.amount,
+    this.image,
+    this.seller,
+    this.height,
+  });
+}
