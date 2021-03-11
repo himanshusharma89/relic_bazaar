17754a51002c17ecf3802f565f637660eb88b8e7
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-03-12 00:01:37 +0530

    added widget tests for ProductCard, ProductPage and BottomNavigationBar

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,107 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:retro_shopping/helpers/app_icons.dart';
+import 'package:retro_shopping/helpers/constants.dart';
 
 import 'package:retro_shopping/main.dart';
+import 'package:retro_shopping/widgets/bottom_nav_bar.dart';
+import 'package:retro_shopping/widgets/product/product_card.dart';
+import 'package:retro_shopping/widgets/product/product_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  //test for ProductCard widget
+  testWidgets(
+    "ProductCard displays text and image correctly",
+    (WidgetTester tester) async {
+      await tester.pumpWidget(Directionality(
+        textDirection: TextDirection.ltr,
+        child: ProductCard(
+          text: 'lorem',
+          owner: 'ipsum',
+          amount: 'dolor',
+          image: 'assets/items/3.png',
+          seller: 'seller',
+          height: 10,
+        ),
+      ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      expect(find.text('lorem'), findsOneWidget);
+      expect(find.text('ipsum'), findsOneWidget);
+      expect(find.text('dolor'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      //TODO: Add test for AssetImage
+    },
+  );
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  //test for ProductPage widget
+  testWidgets(
+    "ProductPage displays text and image correctly",
+    (WidgetTester tester) async {
+      await tester.pumpWidget(MediaQuery(
+        data: new MediaQueryData(),
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: ProductPage(
+            text: 'text',
+            image: 'assets/items/3.png',
+            owner: 'owner',
+            amount: 'amount',
+            prodHeight: 10,
+            seller: 'seller',
+          ),
+        ),
+      ));
+
+      expect(find.text('owner'), findsOneWidget);
+      expect(find.text('amount'), findsOneWidget);
+      expect(find.text('10'), findsOneWidget);
+      expect(find.text('seller'), findsOneWidget);
+    },
+  );
+
+  //test for bottomNavigationBar widget
+  testWidgets(
+    "BottomNavigationBar works correctly",
+    (WidgetTester tester) async {
+      int index;
+      final PageController _controller = new PageController();
+
+      await tester.pumpWidget(
+        MediaQuery(
+          data: new MediaQueryData(),
+          child: MaterialApp(
+            home: Scaffold(
+              bottomNavigationBar: BottomNavigationBar(
+                items: [
+                  BottomNavigationBarItem(
+                    icon: Icon(RelicIcons.home),
+                    label: 'home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(RelicIcons.search),
+                    label: 'search',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(RelicIcons.cart),
+                    label: 'cart',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(RelicIcons.profile),
+                    label: 'profile',
+                  ),
+                ],
+                onTap: (value) {
+                  index = value;
+                },
+              ),
+            ),
+          ),
+        ),
+      );
+
+      await tester.tap(find.byIcon(RelicIcons.search));
+      expect(index, 1);
+    },
+  );
 }
