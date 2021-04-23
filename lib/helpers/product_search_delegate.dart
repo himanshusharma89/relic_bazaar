3b4a3a1817e11b8c631e580b22a07d0ce1c9c85b
Sapinder Singh <sapindersingh1231@gmail.com>
2021-04-23 12:02:50 +0530

    Fixes #25 (#204)
    * Fixes #25
    
    * Fetching products using FutureBuilder.
    
    * Restored the import. Added uniform padding.

---
+++ lib/helpers/product_search_delegate.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:retro_shopping/helpers/constants.dart';
+import 'package:retro_shopping/model/product_model.dart';
+import 'package:retro_shopping/services/product_service.dart';
+import 'package:retro_shopping/widgets/product/product_card.dart';
+
+class ProductSearchDelegate extends SearchDelegate<ProductCard> {
+  @override
+  String get searchFieldLabel => 'Search for products';
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: const Icon(Icons.close),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return ThemeData(
+      textSelectionTheme: const TextSelectionThemeData(
+        cursorColor: Colors.white,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: RelicColors.backgroundColor,
+      ),
+    );
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final ProductService productService = ProductService();
+    return FutureBuilder<List<ProductCard>>(
+      future: productService.getProducts(),
+      builder: (_, AsyncSnapshot<List<ProductCard>> snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          final List<ProductCard> productList = snapshot.data
+              .where(
+                (ProductCard element) =>
+                    element.product.text.toLowerCase().contains(query),
+              )
+              .toList();
+
+          return productList.isEmpty
+              ? const Center(
+                  child: Text(
+                    'This product is not available !!',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              : ListView.separated(
+                  itemBuilder: (_, int i) {
+                    final Product product = productList[i].product;
+                    return InkWell(
+                      onTap: () => Navigator.of(context).pushNamed(
+                        RouteConstant.PRODUCTS_SCREEN,
+                        arguments: Product(
+                          amount: product.amount,
+                          height: product.height,
+                          image: product.image,
+                          owner: product.owner,
+                          seller: product.seller,
+                          text: product.text,
+                        ),
+                      ),
+                      child: ListTile(
+                        title: Text(productList[i].product.text),
+                        leading: CircleAvatar(
+                          backgroundColor: Colors.white,
+                          backgroundImage:
+                              AssetImage(productList[i].product.image),
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (_, int i) => const Divider(),
+                  itemCount: productList.length,
+                );
+        }
+      },
+    );
+  }
+}
--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:retro_shopping/helpers/app_icons.dart';
-import 'package:retro_shopping/helpers/constants.dart';
-import 'package:retro_shopping/widgets/retro_button.dart';
+import 'package:retro_shopping/widgets/search_bar.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -12,30 +10,12 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
-    final double width = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 12),
-        child: Column(
-          children: <Widget>[
-            RelicBazaarStackedView(
-              height: height * 0.06,
-              width: width * 0.9,
-              upperColor: Colors.white,
-              child: TextFormField(
-                style: const TextStyle(
-                  fontFamily: 'pix M 8pt',
-                  fontSize: 18,
-                  color: RelicColors.primaryBlack,
-                ),
-                decoration: const InputDecoration(
-                    hintText: 'search for categories, items and more...',
-                    border: InputBorder.none,
-                    prefixIcon: Icon(RelicIcons.search)),
-              ),
-            ),
-          ],
+        padding: EdgeInsets.all(
+          height / 40,
         ),
+        child: searchBar(context),
       ),
     );
   }
