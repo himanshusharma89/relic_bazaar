cb10288236f3bbb90aded992567d897825ded99d
Sapinder Singh <sapindersingh1231@gmail.com>
2021-05-29 19:09:46 +0530

    Fixes #244 (#245)

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -36,31 +36,44 @@ class AuthenticationService {
   }
 
   static Future<String> signInWithGoogle() async {
-    final GoogleSignInAccount googleSignInAccount =
-        await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication googleSignInAuthentication =
-        await googleSignInAccount.authentication;
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+          await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
 
-    final AuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleSignInAuthentication.accessToken,
-      idToken: googleSignInAuthentication.idToken,
-    );
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
 
-    final UserCredential authResult =
-        await FirebaseAuth.instance.signInWithCredential(credential);
-    final User user = authResult.user;
-    assert(!user.isAnonymous);
+      final UserCredential authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      final User user = authResult.user;
+      assert(!user.isAnonymous);
 
-    final User currentUser = FirebaseAuth.instance.currentUser;
-    assert(user.uid == currentUser.uid);
-    await addUserToFirebase(uid: user.uid, email: user.email);
-    await updateUserInFirebase(
-      uid: user.uid,
-      imageUrl: user.photoURL,
-      phoneNumber: user.phoneNumber,
-      userName: user.displayName,
-    );
-    return '$user';
+      final User currentUser = FirebaseAuth.instance.currentUser;
+      assert(user.uid == currentUser.uid);
+      await addUserToFirebase(uid: user.uid, email: user.email);
+      await updateUserInFirebase(
+        uid: user.uid,
+        imageUrl: user.photoURL,
+        phoneNumber: user.phoneNumber,
+        userName: user.displayName,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    } catch (e) {
+      String errorText;
+      if (e.toString().contains(
+          "NoSuchMethodError: The getter 'authentication' was called on null.")) {
+        errorText = 'Sign In Cancelled By User';
+      } else if (e.toString().contains('network_error')) {
+        errorText = 'Please check your network connection.';
+      }
+      return errorText;
+    }
   }
 
   static Future<void> signOutGoogle() async {
